63d2764784dab1aa5fbce40adf78cf031ff341e0
Korotchenko <kroha2007.85@gmail.com>
2025-02-14 14:58:03 +0200

    Add gitaction

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/sbin/php-fpm -D; cd /var/www/html; php artisan migrate; php artisan config:clear; php artisan cache:clear; php artisan route:clear; php artisan view:clear; php artisan storage:link;  sleep 10; chown www-data: /var/www/html/storage/logs/laravel.log; chown www-data: /var/www/html/storage/framework/cache/data; nginx -g 'daemon off;'
+/usr/local/sbin/php-fpm -D; cd /var/www/html; php artisan key:generate; php artisan migrate; php artisan config:clear; php artisan cache:clear; php artisan route:clear; php artisan view:clear; php artisan storage:link;  sleep 10; chown www-data: /var/www/html/storage/logs/laravel.log; chown www-data: /var/www/html/storage/framework/cache/data; chmod -R 777 /var/www/html/database; nginx -g 'daemon off;'
 
 exec "$@"
